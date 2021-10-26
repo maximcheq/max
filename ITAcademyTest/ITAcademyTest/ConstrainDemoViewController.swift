5e9c7b92d0893c338dc4165f73a150f45cec4101
maximcheq <mkuklitskij@gmail.com>
2021-10-26 21:46:20 +0300

    autolayout

---
+++ ITAcademyTest/ITAcademyTest/ConstrainDemoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConstrainDemoViewController.swift
+//  ITAcademyTest
+//
+//  Created by Максим  on 26.10.21.
+//
+
+import UIKit
+
+class ConstrainDemoViewController: UIViewController {
+    
+}
+
