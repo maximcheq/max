e730d71434410a542f62ce5fb73404d7a37dc677
maximcheq <mkuklitskij@gmail.com>
2021-11-29 20:54:48 +0300

    added new tableView with the unique names chosen in previous tableView

--- StudentList/StudentList/SelectedStudentViewController.swift
+++ StudentList/StudentList/SelectedStudentViewController.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class SelectedStudentViewController: UIViewController {
     
+    var setStudentList = Set<String>()
+    var arrayStudentList: [String] = []
+    
     @IBOutlet weak var selectedButton: UIButton!
+    
+    @IBOutlet weak var tableView: UITableView!
     
     @IBAction func selectedButtonTapped(_ sender: Any) {
         
@@ -17,17 +22,42 @@ class SelectedStudentViewController: UIViewController {
         let vc = storyboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
         vc.delegate = self
         present(vc, animated: true, completion: nil)
-        
     }
     
 }
 
 extension SelectedStudentViewController: ViewControllerDelegate {
     func didSelectStudent(_ student: String) {
-        selectedButton.setTitle(student, for: .normal)
+        setStudentList.insert(student)
+        tableView.reloadData()
         dismiss(animated: true, completion: nil)
     }
     
-    
+}
+
+extension SelectedStudentViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        setStudentList.count
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if setStudentList.isEmpty {
+            return nil
+        } else {
+        return "Количиство: \(arrayStudentList.count)"
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        arrayStudentList = Array(setStudentList).sorted()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StudentCell1", for: indexPath) as! StudentCell1
+        cell.labelName.text = arrayStudentList[indexPath.row]
+
+        return cell
+    }
+
+
 }
 
---
+++ StudentList/StudentList/StudentCell1.swift
@@ -0,0 +1,14 @@
+//
+//  StudentCell1.swift
+//  StudentList
+//
+//  Created by Максим  on 29.11.21.
+//
+
+import UIKit
+
+class StudentCell1: UITableViewCell {
+
+    @IBOutlet weak var labelName: UILabel!
+    
+}
